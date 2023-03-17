f81c4e0a850630ced7bdb850627a41c7ae4d535a
Stefan Balaban <stefan@madball.ro>
2023-03-17 12:57:50 +0200

    features

---
+++ node/feature_extractor.js
@@ -0,0 +1,53 @@
+const constants = require('../common/constants');
+const features = require('../common/features');
+
+const fs = require('fs');
+console.log("Extracting features...");
+
+const samples = JSON.parse(
+    fs.readFileSync(constants.SAMPLES)
+);
+
+console.log(samples.length);
+let i = 0;
+
+for (const sample of samples) {
+    i++;
+    if((i/samples.length*100).toFixed((0)) != ((i - 1)/samples.length*100).toFixed((0))) {
+        console.log((i/samples.length*100).toFixed((0)) + "%");
+    }
+    const paths = JSON.parse(
+        fs.readFileSync(
+            constants.JSON_DIR + "/" + sample.id + ".json"
+        )
+    );
+    sample.point = [
+        features.getPathCount(paths),
+        features.getPointCount(paths)
+    ];
+}
+
+const featuresNames = [
+    "Path Count",
+    "Point Count"
+];
+
+fs.writeFileSync(constants.FEATURES,
+    JSON.stringify({
+        featuresNames,
+        samples:samples.map(
+            s => {
+                return {
+                    point:s.point,
+                    label:s.label
+                }
+            }
+        )
+    })
+);
+
+fs.writeFileSync(constants.FEATURES_JS,
+    `const features = ${JSON.stringify({featuresNames, samples})};`   
+ );
+
+console.log("Done");
